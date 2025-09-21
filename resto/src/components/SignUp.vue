375ed2dc5c13196a36d2ac81e6477f6fd1a6553c
Sukhmeet-0 <xsukhmeet@gmail.com>
2025-09-21 23:21:45 +0530

    adding sign up page

---
+++ resto/src/components/SignUp.vue
@@ -0,0 +1,50 @@
+<script>
+    export default{
+        name:"SignUp"
+    }
+</script>
+<template>
+    <img src="../assets/logo.png" alt="" class="logo">
+    <h1>Sign Up</h1>
+    <div class="register">
+        <input type="text" placeholder="Enter name"/>
+        <input type="email" placeholder="Enter email"/>
+        <input type="password" placeholder="Enter password">
+        <button>Sign Up</button>
+    </div>
+</template>
+<style>
+    .logo{
+        width: 200px;
+       
+    }
+    .register input{
+        width: 300px;
+        height: 40px;
+        padding-left: 20px;
+        display: block;
+        margin-bottom: 30px;
+        margin-right: auto;
+        margin-left: auto;
+        border: 1px solid red;
+        border-radius: 10px;
+    }
+    .register button{
+        width: 320px;
+        height: 40px;
+        padding-left: 20px;
+        display: block;
+        margin-bottom: 30px;
+        margin-right: auto;
+        margin-left: auto;
+        background: red;
+        color: white;
+        cursor: pointer; 
+        border: none;
+        font-size: large;
+        border-radius: 10px;
+    }
+    .register button:hover{
+        background: rgb(150, 36, 36);
+    }
+</style>
